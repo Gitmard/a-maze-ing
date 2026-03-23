4cfdfa298d1fd12ffde14c94a61c498a700daa75
Gerard2par2 <floogles71@gmail.com>
2026-03-23 15:11:43 +0100

    chore: comment code

--- src/generator/RecursiveDivisionGenerator.py
+++ src/generator/RecursiveDivisionGenerator.py
@@ -112,18 +112,26 @@ class RecursiveDivisionGenerator(MazeGenerator):
         self,
         current_frame: DivisionFrame
     ) -> List[DivisionFrame]:
+
+        # Choose the direction of the slice
+        # If the current slice is taller than it is wide,
+        # Slice horizontally
         if current_frame.local_height > current_frame.local_width:
             return self.__slice_random(
                 current_frame,
                 RecursiveDivisionGenerator.Direction.HORIZONTAL
             )
 
+        # The current slice is wider than it is tall,
+        # Slice vertically
         if current_frame.local_height < current_frame.local_width:
             return self.__slice_random(
                 current_frame,
                 RecursiveDivisionGenerator.Direction.VERTICAL
             )
 
+        # The current slice is as wide as it is tall
+        # Pick a random direction
         return self.__slice_random(
             current_frame,
             (
@@ -139,24 +147,38 @@ class RecursiveDivisionGenerator(MazeGenerator):
         current_frame: DivisionFrame
     ) -> List[Cell]:
         edited_cells: List[Cell] = []
+
+        # The wall's x coordinate is constant
         wall_x = current_frame.subregion_pos.x + current_frame.local_width - 1
+
+        # Choose a random coordinate for the opening
+        # We bound the coordinate from the beggining to the height - 2
+        # For a slice of width 10, the opening will be between y=0 and y=8
         opening_y = self._get_rng().randint(
             current_frame.subregion_pos.y,
             current_frame.local_height - 2
         )
+
+        # Pick a new coordinate if we chose a locked cell
         while self.get_maze().map[opening_y][wall_x].locked:
             opening_y = self._get_rng().randint(
                 current_frame.subregion_pos.y,
                 current_frame.local_height - 2
             )
+
+        # Loop through the wall's height
         for y in range(current_frame.local_height):
             curr_cells = [
                 self.get_maze().map[y][wall_x],
                 self.get_maze().map[y][wall_x + 1]
             ]
+
+            # Update the cells adjacent to the wall
             curr_cells[0].enclose(EDirection.EAST)
             curr_cells[1].enclose(EDirection.WEST)
             edited_cells.extend(curr_cells)
+
+        # Carve the cells with the opening
         self.get_maze()[opening_y][wall_x].carve(EDirection.EAST)
         self.get_maze()[opening_y][wall_x + 1].carve(EDirection.WEST)
         return edited_cells
@@ -166,24 +188,41 @@ class RecursiveDivisionGenerator(MazeGenerator):
         current_frame: DivisionFrame
     ) -> List[Cell]:
         edited_cells: List[Cell] = []
+
+        # The wall's Y coordinate is constant
         wall_y = current_frame.subregion_pos.y + current_frame.local_height - 1
+
+        # Choose a random coordinate for the opening
+        # We bound the coordinate from the beggning to the width - 2
+        # For a slice of width 10, the opening will be between x=0 and x=8
         opening_x = self._get_rng().randint(
             current_frame.subregion_pos.x,
             current_frame.local_width - 2
         )
-        while self.get_maze().map[wall_y][opening_x].locked:
+
+        # Pick a new coordinate if we chose a locked cell
+        while (
+            self.get_maze().map[wall_y][opening_x].locked
+            or self.get_maze().map[wall_y][opening_x + 1].locked
+        ):
             opening_x = self._get_rng().randint(
                 current_frame.subregion_pos.x,
                 current_frame.local_width - 2
             )
+
+        # Loop through the wall's width
         for x in range(current_frame.local_width):
             curr_cells = [
                 self.get_maze().map[wall_y][x],
                 self.get_maze().map[wall_y + 1][x]
             ]
+
+            # Update the cells adjacent to the wall
             curr_cells[0].enclose(EDirection.SOUTH)
             curr_cells[1].enclose(EDirection.NORTH)
             edited_cells.extend(curr_cells)
+
+        # Carve the cells with the opening
         self.get_maze()[wall_y][opening_x].carve(EDirection.SOUTH)
         self.get_maze()[wall_y][opening_x + 1].carve(EDirection.NORTH)
         return edited_cells
@@ -208,31 +247,19 @@ class RecursiveDivisionGenerator(MazeGenerator):
         List[List[Cell]],
         Generator[List[Cell], None, None],
     ]:
-        """
-            stack = [full_maze]
-            while stack:
-                curr = stack.pop()
-                if max(curr.height, curr.width) <= 2:
-                    continue
-
-                decide which slice
-
-                slice_random()
-
-                create_wall
-
-                stack.append(slice1)
-                stack.append(slice2)
-        """
+        # TODO: check the tick_count parameter and yield if it is defined
         if seed is not None:
             self._get_rng().seed(seed)
 
+        # Initialize the stack with a frame of the full maze
         stack: List[RecursiveDivisionGenerator.DivisionFrame] = [
             RecursiveDivisionGenerator.DivisionFrame(
                 position=(
-                    RecursiveDivisionGenerator.Position.NONE
+                    # Placeholder value, the full maze has no "position"
+                    RecursiveDivisionGenerator.Position.TOP
                 ),
                 direction=(
+                    # Placeholder value, the full maze has no "direction"
                     RecursiveDivisionGenerator.Direction.NONE
                 ),
                 local_height=self.get_maze().height,
@@ -243,22 +270,28 @@ class RecursiveDivisionGenerator(MazeGenerator):
 
         updated_cells: List[Cell] = []
         while len(stack):
+            # Get the frame to process
             current_frame = stack.pop()
 
+            # Stop condition, the frame will be skipped if too small
+            # TODO: keep slicing if one of the dimensions still allows it
             if max(
                 current_frame.local_height,
                 current_frame.local_width
             ) <= 2:
                 continue
 
+            # Slice 2 new frames
             new_frames: List[
                 RecursiveDivisionGenerator.DivisionFrame
             ] = self.__create_new_frames(current_frame)
 
+            # Add the wall for the current frame
             updated_cells.extend(
                 self.__add_wall(current_frame)
             )
 
+            # Add the new frames to the stack
             stack.extend(new_frames)
 
         return updated_cells
