506301510c81e936d031b477a60a8850f65e3a99
smenard <smenard@student.42lyon.fr>
2026-04-02 16:41:58 +0200

    fix: annotate params in calls from ConfigValidator.validate

--- shared/ConfigValidator.py
+++ shared/ConfigValidator.py
@@ -100,8 +100,14 @@ class ConfigValidator:
         output_file: str,
         seed: Optional[str]
     ) -> None:
-        cls.__validate_maze_dimensions(height, width)
-        cls.__validate_entry_exit(entry, exit, height, width)
+        cls.__validate_maze_dimensions(
+            height=height,
+            width=width)
+        cls.__validate_entry_exit(
+            entry=entry,
+            exit=exit,
+            height=height,
+            width=width)
         cls.__validate_perfect(perfect)
         cls.__validate_output_file(output_file)
         cls.__validate_seed(seed)
